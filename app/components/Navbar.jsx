9ac52ece1de9669db17eb5fd1bf0036cd61fa70d
Allison Zhao <allisonzhao.uni@gmail.com>
2025-10-09 00:00:07 -0400

    add navbar basics

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <nav>
+            <a href="">
+                
+            </a>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+            <div>
+                <a href="#contact">Contact</a>
+            </div>
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
