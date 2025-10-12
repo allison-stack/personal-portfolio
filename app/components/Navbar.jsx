a52f93f4c01f2ea4f93674ef538dee3b518a038f
Allison Zhao <allisonzhao.uni@gmail.com>
2025-10-11 23:52:18 -0400

    add menu and light/dark mode buttons

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FaMoon } from 'react-icons/fa';
+import { MdOutlineMenu } from "react-icons/md";
 
 const Navbar = () => {
   return (
@@ -7,14 +9,20 @@ const Navbar = () => {
             <a href="#top">
                 
             </a>
-            <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3'>
+            <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
-            <div>
+            <div className='flex items-center gap-4'>
+                <button>
+                    <FaMoon />
+                </button>
                 <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4'>Contact</a>
+                <button className='block md:hidden ml-3'>
+                    <MdOutlineMenu />
+                </button>
             </div>
         </nav>
     </>
