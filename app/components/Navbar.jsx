b9da08cc71e886fa443f9cbacbb5df6ae5b65fde
Allison Zhao <allisonzhao.uni@gmail.com>
2025-10-12 23:02:29 -0400

    add menu functionality

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import { FaMoon } from 'react-icons/fa';
 import { MdOutlineMenu } from "react-icons/md";
+import { IoIosClose } from "react-icons/io";
 
 const Navbar = () => {
+
+    const sideMenuRef = useRef();
+    const openMenu = () => {
+        sideMenuRef.current.style.transform = 'translateX(-16rem)';
+    }
+    const closeMenu = () => {
+        sideMenuRef.current.style.transform = 'translateX(16rem)';
+    }
+
   return (
     <>
         <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
@@ -20,10 +30,20 @@ const Navbar = () => {
                     <FaMoon />
                 </button>
                 <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4'>Contact</a>
-                <button className='block md:hidden ml-3'>
-                    <MdOutlineMenu />
+                <button className='block md:hidden ml-3' onClick={openMenu}>
+                    <MdOutlineMenu className='h-7 w-7' />
                 </button>
             </div>
+
+            <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+                <div className='absolute right-6 top-6' onClick={closeMenu}>
+                    <IoIosClose className='h-10 w-10 cursor-pointer' />
+                </div>
+                <li><a onClick={closeMenu} href="#home">Home</a></li>
+                <li><a onClick={closeMenu} href="#about">About</a></li>
+                <li><a onClick={closeMenu} href="#projects">Projects</a></li>
+                <li><a onClick={closeMenu} href="#contact">Contact</a></li>
+            </ul>
         </nav>
     </>
   )
