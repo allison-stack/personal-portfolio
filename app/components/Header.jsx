c4a2d16edc67aa071b3121187466c0ea2dc30fe2
Allison Zhao <allisonzhao.uni@gmail.com>
2025-10-14 23:51:41 -0400

    finish header stuff

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
+    <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>
         <div>
             
         </div>
